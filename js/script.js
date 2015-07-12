4d0d5c06b82d2aa41eb63df4d36a545684e06a38
Mikel Severson <mikelseverson@Mikels-MacBook-Air.local>
2015-07-12 01:30:01 -0500

    Toggle to hide resume added - removed extra script.js call in index.html

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@
  * Created by mikelseverson on 7/1/15.
  */
 $(document).ready(function() {
-    $(".show-resume").on('click', function(event) {
-        event.preventDefault();
-        $('.card').slideDown();
+    $(".show-resume").on('click', function() {
+        $('.card').slideToggle();
     });
 });
