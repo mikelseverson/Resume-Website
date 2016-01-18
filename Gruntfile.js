40e0352f868dd658e1c988a6a008449b46135901
Mikel Severson <mkseve@gmail.com>
2016-01-17 18:36:47 -0600

    testing https

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,51 +15,6 @@ module.exports = function(grunt){
             }
         },
         copy: {
-            angular: {
-                expand: true,
-                cwd: 'node_modules',
-                src: [
-                    "angular/angular.min.js",
-                    "angular/angular.min.js.map"
-                ],
-                "dest": "server/public/vendors/"
-            },
-            angularRoutes: {
-                expand: true,
-                cwd: 'node_modules',
-                src: [
-                    "angular-route/angular-route.min.js",
-                    "angular-route/angular-route.min.js.map"
-                ],
-                "dest": "server/public/vendors/"
-            },
-            angularAria: {
-                expand: true,
-                cwd: 'node_modules',
-                src: [
-                    "angular-aria/angular-aria.min.js",
-                    "angular-aria/angular-aria.min.js.map"
-                ],
-                "dest": "server/public/vendors/"
-            },
-            angularMaterial: {
-                expand: true,
-                cwd: 'node_modules',
-                src: [
-                    "angular-material/angular-material.min.css",
-                    "angular-material/angular-material.min.js"
-                ],
-                "dest": "server/public/vendors/"
-            },
-            angularAnimate: {
-                expand: true,
-                cwd: 'node_modules',
-                src: [
-                    "angular-animate/angular-animate.min.js",
-                    "angular-animate/angular-animate.min.js.map"
-                ],
-                "dest": "server/public/vendors/"
-            },
             style: {
                 expand: true,
                 cwd: 'client',
@@ -81,14 +36,6 @@ module.exports = function(grunt){
                 cwd: 'client',
                 src: [
                     "views/*"
-                ],
-                "dest": "server/public/assets"
-            },
-            htmlRoutes: {
-                expand: true,
-                cwd: 'client/',
-                src: [
-                    "views/routes/*"
                 ],
                 "dest": "server/public/assets"
             }
--- server/app.js
+++ server/app.js
@@ -1,16 +1,42 @@
-/**
- * Created by mikelseverson on 8/20/15.
- */
+'use strict';
+
 var express = require('express');
 var app = express();
+var lex = require('letsencrypt-express');
+
 
 var index = require('./routes/index');
 
 app.use('/', index);
 app.set("port", (process.env.PORT || 5000));
 
-app.listen(app.get("port"), function(){
-    console.log("Listening on port: " + app.get("port"));
-});
+// app.listen(app.get("port"), function(){
+//     console.log("Listening on port: " + app.get("port"));
+// });
+
+var results = lex.create({
+  configDir: '/etc/letsencrypt'
+, onRequest: app
+, server: require('letsencrypt').productionServerUrl
+}).listen(
+
+  // you can give just the port, or expand out to the full options
+  [80, { port: 8080, address: 'localhost', onListening: function () { console.log('http://localhost'); } }]
+
+  // you can give just the port, or expand out to the full options
+, [443, 5001, { port: 8443, address: 'localhost' }]
+
+  // this is pretty much the default onListening handler
+, function onListening() {
+    var server = this;
+    var protocol = ('requestCert' in server) ? 'https': 'http';
+    console.log("Listening at " + protocol + '://localhost:' + this.address().port);
+  }
+);
+
+// In case you need access to the raw servers (i.e. using websockets)
+console.log(results.plainServers);
+console.log(results.tlsServers);
+
 
 module.exports = app;
