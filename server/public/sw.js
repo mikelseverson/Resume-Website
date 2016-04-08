46848dd0acb750759d0662b2894f8fb613e58c10
Mikel Severson <mkseve@gmail.com>
2016-04-08 12:35:03 -0500

    removed hotjar

--- server/public/sw.js
+++ server/public/sw.js
@@ -34,8 +34,6 @@ self.addEventListener('activate', function(event) {
   );
 });
 
-
-
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
